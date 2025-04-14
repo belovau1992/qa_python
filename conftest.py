820487178afb8a14241f1ce58417dd93911fabe1
Andrew Below <belovau1992@yandex.com>
2025-04-14 15:03:24 +0300

    delete fixture collector_with_favorite in conftest.py

--- conftest.py
+++ conftest.py
@@ -8,8 +8,3 @@ from main import BooksCollector
 def collector():
     collector = BooksCollector()
     return collector
-
-@pytest.fixture()
-def collector_with_favorites(collector):
-    collector.add_book_in_favorites(generate_random_book_title())
-    return collector
